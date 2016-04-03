65cd612aac54d21096cb4c94a63a769c2bbfa8e0
kbarbounakis <k.barbounakis@gmail.com>
2016-04-04 00:15:02 +0300

    most php client update (version 2.0.1)

--- test/TestConfig.php
+++ test/TestConfig.php
@@ -1,5 +1,5 @@
 <?php
 
-define('REMOTE_URL', "http://127.0.0.1");
+define('REMOTE_URL', "http://127.0.0.1:3000");
 define('REMOTE_USER', "alexis.rees@example.com");
 define('REMOTE_PASSWORD', "user");
